93fc03c04314363c3461df2ce457ac89cee6cfc5
Katschinga <39401974+Katschinga@users.noreply.github.com>
2018-05-27 18:33:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Create an object and an array which we will use in our facebook exercise. 
-
-// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
-
-
-// 2. Create an array which contains the object you have made above and name the array "database".
-
-// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
-
 var database = [
 	{
 		username: "Andy",
@@ -57,5 +48,3 @@ function signIn(user, pass) {
 		alert("Sorry, wrong username and/or password");
 	}
 }
-
-signIn(userNamePrompt, passwordPrompt);
